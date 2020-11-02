d4b88f5eaced502d52bc879a347a04f5229a2dbb
Ruud Gerrits <ruud@302.nl>
2020-11-02 10:32:30 +0100

    Added events

--- src/components/vue-dropzone.vue
+++ src/components/vue-dropzone.vue
@@ -265,6 +265,14 @@ export default {
       vm.$emit("vdropzone-drag-leave", event);
     });
 
+    this.dropzone.on("init", function() {
+      vm.$emit("vdropzone-init");
+    });
+
+    this.dropzone.on("chunksUploaded", function(file, done) {
+      vm.$emit("vdropzone-chunks-uploaded", file, done);
+    });
+
     vm.$emit("vdropzone-mounted");
   },
   beforeDestroy() {
